3402e31c4054971228c734000646b95f8eb22337
Tuấn Trần <guithutuantran@gmail.com>
2025-06-09 08:14:28 +0000

    Use font "Segoe UI"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Segoe UI', 'sans-serif'],
+        headline: ['Segoe UI', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
