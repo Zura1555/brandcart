bb5c947e1ec6ddbd25e3d740d7cdc88e5b62445b
Tuấn Trần <guithutuantran@gmail.com>
2025-06-09 08:31:01 +0000

    Add more sample: Category	Element	Information to Input Item 1 Details	Br

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -11,7 +11,7 @@ export const mockShops: Shop[] = [
         price: 1890000,
         brand: 'MLB',
         imageUrl: 'https://placehold.co/80x80.png',
-        dataAiHint: 'tshirt blue',
+        dataAiHint: 'tshirt lightblue',
         variant: 'Light Blue',
         // originalPrice, stock, discountDescription not specified, so omitted
       },
@@ -28,4 +28,43 @@ export const mockShops: Shop[] = [
     ],
     // isFavorite, promotionText, editLinkText not specified, so omitted
   },
+  {
+    name: 'PUMA',
+    products: [
+      {
+        id: 'puma1',
+        name: 'Balo unisex phom chữ nhật hiện đại',
+        price: 769000,
+        brand: 'PUMA',
+        imageUrl: 'https://placehold.co/80x80.png',
+        dataAiHint: 'backpack darkgrey',
+        variant: 'Dark Grey',
+        discountDescription: '-30%',
+        // originalPrice not specified
+      },
+      {
+        id: 'puma2',
+        name: 'Giày sneakers nữ cổ thấp Mayze',
+        price: 2029000,
+        originalPrice: 2899000,
+        brand: 'PUMA',
+        imageUrl: 'https://placehold.co/80x80.png',
+        dataAiHint: 'sneakers white',
+        variant: 'White/Black',
+        discountDescription: '-30%',
+      },
+      {
+        id: 'puma3',
+        name: 'Nón bóng chày unisex Metal Cat',
+        price: 299000,
+        originalPrice: 599000,
+        brand: 'PUMA',
+        imageUrl: 'https://placehold.co/80x80.png',
+        dataAiHint: 'cap olivegreen',
+        variant: 'Olive Green',
+        discountDescription: '-50%',
+      },
+    ],
+    // isFavorite, promotionText, editLinkText not specified for PUMA
+  }
 ];
