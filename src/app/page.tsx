630e5b90fe623ac48d6b7d97abb18cfd6abe7af1
Tuấn Trần <guithutuantran@gmail.com>
2025-06-13 07:58:05 +0000

    For the shipping discount, dont make the content overflow, make it in on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -541,10 +541,17 @@ const BrandCartPage = () => {
       setCartItems(prevItems => {
         const updatedItems = [...prevItems];
         const brandOfNewItem = newCartItem.brand;
-        const indexToInsert = updatedItems.findIndex(item => item.brand === brandOfNewItem);
+        
+        let insertAtIndex = -1;
+        for(let i=0; i < updatedItems.length; i++){
+            if(updatedItems[i].brand === brandOfNewItem){
+                insertAtIndex = i;
+                break;
+            }
+        }
 
-        if (indexToInsert !== -1) {
-          updatedItems.splice(indexToInsert, 0, newCartItem);
+        if (insertAtIndex !== -1) {
+          updatedItems.splice(insertAtIndex, 0, newCartItem);
         } else {
           updatedItems.push(newCartItem);
         }
@@ -731,7 +738,7 @@ const BrandCartPage = () => {
               </Sheet>
 
              <div className="flex items-center justify-between py-2 cursor-pointer hover:bg-muted/50 -mx-4 px-4">
-                <div className="flex items-center">
+                <div className="flex items-center min-w-0"> {/* Added min-w-0 here */}
                   <Truck className={`w-5 h-5 mr-3 flex-shrink-0 ${truckIconClass}`} />
                   <span className={cn("text-sm truncate", truckIconClass === 'text-green-500' ? 'text-green-600' : 'text-muted-foreground')}>
                     {displayShippingMessage}
