0ee53de8f48026c891c4854769d8967b21bc75d6
Tuấn Trần <guithutuantran@gmail.com>
2025-06-23 04:25:19 +0000

    Remove the Max&Co. from the Recently Viewed carousel, make it appears on

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -258,25 +258,6 @@ export const mockRelevantProducts: Product[] = [
 
 export const newRecentlyViewedProducts: Product[] = [
   {
-    id: 'maxco1',
-    name: 'Áo thun nữ cổ tròn tay ngắn Sanmauro',
-    price: 4590000,
-    brand: 'MAX&Co.',
-    imageUrl: 'https://product.hstatic.net/1000284478/product/1_7114173003_1_62e692c5f196408f9eacab84ac1c91fb_large.jpg', // Main image for cart/details
-    thumbnailImageUrl: 'https://product.hstatic.net/1000284478/product/1_7114173003_1_62e692c5f196408f9eacab84ac1c91fb_large.jpg', // Thumbnail for recently viewed card
-    dataAiHint: 'tshirt white',
-    productCode: 'MAXCO001',
-    variant: 'White, M',
-    stock: 15,
-    availableVariants: [
-      { id: 'maxco1-wh-s', name: 'White, S', stock: 10, price: 4590000, imageUrl: 'https://product.hstatic.net/1000284478/product/1_7114173003_1_62e692c5f196408f9eacab84ac1c91fb_large.jpg', dataAiHint: 'tshirt white' },
-      { id: 'maxco1-wh-m', name: 'White, M', stock: 15, price: 4590000, imageUrl: 'https://product.hstatic.net/1000284478/product/1_7114173003_1_62e692c5f196408f9eacab84ac1c91fb_large.jpg', dataAiHint: 'tshirt white detail' },
-      { id: 'maxco1-bk-m', name: 'Black, M', stock: 5, price: 4590000, imageUrl: 'https://placehold.co/400x400/333333/EFEFEF.png', dataAiHint: 'tshirt black front' },
-      { id: 'maxco1-bk-m-img2', name: 'Black, M', stock: 5, price: 4590000, imageUrl: 'https://placehold.co/400x400/333333/dddddd.png', dataAiHint: 'tshirt black back' },
-    ],
-    discountDescription: 'New Arrival',
-  },
-  {
     id: 'fila1',
     name: 'Áo polo unisex tay ngắn Origin',
     price: 1495000,
@@ -373,3 +354,6 @@ export const newRecentlyViewedProducts: Product[] = [
 
     
 
+
+
+    
